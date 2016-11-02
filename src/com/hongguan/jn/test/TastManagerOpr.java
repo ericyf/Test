f29631a77933decdf2669ebecec9c094abb3b36f
yangfeng <eric_yf@yeah.net>
2016-11-02 11:10:48 +0800

    first version

---
+++ src/com/hongguan/jn/test/TastManagerOpr.java
@@ -0,0 +1,37 @@
+package com.hongguan.jn.test;
+import java.util.ArrayList;
+
+public class TastManagerOpr {
+	
+	public static void main(String[] args) {
+		int [] taskArr = {0,30,155,1,80,255,170,40,99};
+		ArrayList system_task = new ArrayList();
+		ArrayList user_task = new ArrayList();
+		secheduler(taskArr,taskArr.length,system_task,user_task);
+		System.out.println("system_task="+system_task+" user_task="+user_task);
+	}
+	
+	static void secheduler(int[] task,int taskn,ArrayList a,ArrayList b){
+		int tindex = 0;
+		for(int i=0;i<taskn;i++){
+			tindex = 0;
+			for(int j=1;j<taskn;j++){
+				if(task[tindex]>task[j]){
+					tindex=j;
+				}
+			}
+			
+			if(task[tindex]<50){
+				a.add(tindex);
+			}else if(task[tindex]<=255){
+				b.add(tindex);
+			}else{
+				continue;
+			}
+			task[tindex]=256;
+		}
+		
+		a.add(-1);
+		b.add(-1);
+	}
+}
